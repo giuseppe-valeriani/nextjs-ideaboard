fb154bbaff318ec8dfd81869acb72fd2836db584
Naryond <giuggey85@gmail.com>
2022-12-12 04:28:34 +0000

    .

--- components/tile.tsx
+++ components/tile.tsx
@@ -81,7 +81,7 @@ const Tile = ({ card, cancel, editCard }: Props) => {
             style={{
               minWidth: '150px',
               minHeight: '100px',
-              width: '300px',
+              width: '350px',
             }}
           >
             <Card.Header className="card-title">{title}</Card.Header>
