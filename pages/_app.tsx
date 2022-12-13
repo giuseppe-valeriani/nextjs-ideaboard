c1adfb9ba734d3bd1efa6797eea9b8d3e17e4ecc
Naryond <giuggey85@gmail.com>
2022-12-13 22:21:35 +0000

    Page title set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Idea Board</title>
+      </Head>
       <Component {...pageProps} />
     </>
   );
